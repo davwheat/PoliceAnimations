b5cc0d1bc9c681974a5aa4a71c7c3ccdf93429c4
David Wheatley <blokmaster.help@gmail.com>
2018-02-26 13:47:42 +0000

    Merged

--- PoliceAnimations/server.lua
+++ PoliceAnimations/server.lua
@@ -3,5 +3,8 @@ EventHandler('chatMessage', function(source, n, msg)
   if msg == "/k" or msg == "/kneel" then
     CancelEvent()
     TriggerClientEvent('aa:kneelhu', source)
+  elseif msg == "/r" or msg == "/radio" then
+    CancelEvent()
+    TriggerClientEvent('aa:radio', source)
   end
 end)
